e3b34aa8e354a2939ad42a4c7a05208e4e713f14
Administrator <Administrator@DPC32>
2020-10-19 13:00:47 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.vb
@@ -0,0 +1,251 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.panelDO = New System.Windows.Forms.Panel()
+        Me.txtCh5 = New System.Windows.Forms.TextBox()
+        Me.btnCh5 = New System.Windows.Forms.Button()
+        Me.txtCh4 = New System.Windows.Forms.TextBox()
+        Me.btnCh4 = New System.Windows.Forms.Button()
+        Me.txtCh3 = New System.Windows.Forms.TextBox()
+        Me.btnCh3 = New System.Windows.Forms.Button()
+        Me.txtCh2 = New System.Windows.Forms.TextBox()
+        Me.btnCh2 = New System.Windows.Forms.Button()
+        Me.txtCh1 = New System.Windows.Forms.TextBox()
+        Me.btnCh1 = New System.Windows.Forms.Button()
+        Me.txtCh0 = New System.Windows.Forms.TextBox()
+        Me.btnCh0 = New System.Windows.Forms.Button()
+        Me.buttonStart = New System.Windows.Forms.Button()
+        Me.txtReadCount = New System.Windows.Forms.TextBox()
+        Me.txtModule = New System.Windows.Forms.TextBox()
+        Me.label2 = New System.Windows.Forms.Label()
+        Me.label1 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.panelDO.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'panelDO
+        '
+        Me.panelDO.BackColor = System.Drawing.Color.SkyBlue
+        Me.panelDO.Controls.Add(Me.txtCh5)
+        Me.panelDO.Controls.Add(Me.btnCh5)
+        Me.panelDO.Controls.Add(Me.txtCh4)
+        Me.panelDO.Controls.Add(Me.btnCh4)
+        Me.panelDO.Controls.Add(Me.txtCh3)
+        Me.panelDO.Controls.Add(Me.btnCh3)
+        Me.panelDO.Controls.Add(Me.txtCh2)
+        Me.panelDO.Controls.Add(Me.btnCh2)
+        Me.panelDO.Controls.Add(Me.txtCh1)
+        Me.panelDO.Controls.Add(Me.btnCh1)
+        Me.panelDO.Controls.Add(Me.txtCh0)
+        Me.panelDO.Controls.Add(Me.btnCh0)
+        Me.panelDO.Location = New System.Drawing.Point(257, 104)
+        Me.panelDO.Name = "panelDO"
+        Me.panelDO.Size = New System.Drawing.Size(171, 222)
+        Me.panelDO.TabIndex = 91
+        '
+        'txtCh5
+        '
+        Me.txtCh5.Location = New System.Drawing.Point(83, 190)
+        Me.txtCh5.Name = "txtCh5"
+        Me.txtCh5.Size = New System.Drawing.Size(84, 20)
+        Me.txtCh5.TabIndex = 11
+        '
+        'btnCh5
+        '
+        Me.btnCh5.Location = New System.Drawing.Point(6, 187)
+        Me.btnCh5.Name = "btnCh5"
+        Me.btnCh5.Size = New System.Drawing.Size(75, 25)
+        Me.btnCh5.TabIndex = 10
+        Me.btnCh5.Text = "DO 5"
+        Me.btnCh5.UseVisualStyleBackColor = True
+        '
+        'txtCh4
+        '
+        Me.txtCh4.Location = New System.Drawing.Point(83, 153)
+        Me.txtCh4.Name = "txtCh4"
+        Me.txtCh4.Size = New System.Drawing.Size(84, 20)
+        Me.txtCh4.TabIndex = 9
+        '
+        'btnCh4
+        '
+        Me.btnCh4.Location = New System.Drawing.Point(6, 151)
+        Me.btnCh4.Name = "btnCh4"
+        Me.btnCh4.Size = New System.Drawing.Size(75, 25)
+        Me.btnCh4.TabIndex = 8
+        Me.btnCh4.Text = "DO 4"
+        Me.btnCh4.UseVisualStyleBackColor = True
+        '
+        'txtCh3
+        '
+        Me.txtCh3.Location = New System.Drawing.Point(83, 116)
+        Me.txtCh3.Name = "txtCh3"
+        Me.txtCh3.Size = New System.Drawing.Size(84, 20)
+        Me.txtCh3.TabIndex = 7
+        '
+        'btnCh3
+        '
+        Me.btnCh3.Location = New System.Drawing.Point(6, 114)
+        Me.btnCh3.Name = "btnCh3"
+        Me.btnCh3.Size = New System.Drawing.Size(75, 25)
+        Me.btnCh3.TabIndex = 6
+        Me.btnCh3.Text = "DO 3"
+        Me.btnCh3.UseVisualStyleBackColor = True
+        '
+        'txtCh2
+        '
+        Me.txtCh2.Location = New System.Drawing.Point(83, 79)
+        Me.txtCh2.Name = "txtCh2"
+        Me.txtCh2.Size = New System.Drawing.Size(84, 20)
+        Me.txtCh2.TabIndex = 5
+        '
+        'btnCh2
+        '
+        Me.btnCh2.Location = New System.Drawing.Point(6, 77)
+        Me.btnCh2.Name = "btnCh2"
+        Me.btnCh2.Size = New System.Drawing.Size(75, 25)
+        Me.btnCh2.TabIndex = 4
+        Me.btnCh2.Text = "DO 2"
+        Me.btnCh2.UseVisualStyleBackColor = True
+        '
+        'txtCh1
+        '
+        Me.txtCh1.Location = New System.Drawing.Point(83, 42)
+        Me.txtCh1.Name = "txtCh1"
+        Me.txtCh1.Size = New System.Drawing.Size(84, 20)
+        Me.txtCh1.TabIndex = 3
+        '
+        'btnCh1
+        '
+        Me.btnCh1.Location = New System.Drawing.Point(6, 40)
+        Me.btnCh1.Name = "btnCh1"
+        Me.btnCh1.Size = New System.Drawing.Size(75, 25)
+        Me.btnCh1.TabIndex = 2
+        Me.btnCh1.Text = "DO 1"
+        Me.btnCh1.UseVisualStyleBackColor = True
+        '
+        'txtCh0
+        '
+        Me.txtCh0.Location = New System.Drawing.Point(83, 5)
+        Me.txtCh0.Name = "txtCh0"
+        Me.txtCh0.Size = New System.Drawing.Size(84, 20)
+        Me.txtCh0.TabIndex = 1
+        '
+        'btnCh0
+        '
+        Me.btnCh0.Location = New System.Drawing.Point(6, 3)
+        Me.btnCh0.Name = "btnCh0"
+        Me.btnCh0.Size = New System.Drawing.Size(75, 25)
+        Me.btnCh0.TabIndex = 0
+        Me.btnCh0.Text = "DO 0"
+        Me.btnCh0.UseVisualStyleBackColor = True
+        '
+        'buttonStart
+        '
+        Me.buttonStart.Location = New System.Drawing.Point(349, 5)
+        Me.buttonStart.Name = "buttonStart"
+        Me.buttonStart.Size = New System.Drawing.Size(75, 25)
+        Me.buttonStart.TabIndex = 74
+        Me.buttonStart.Text = "Start"
+        Me.buttonStart.UseVisualStyleBackColor = True
+        '
+        'txtReadCount
+        '
+        Me.txtReadCount.Location = New System.Drawing.Point(101, 43)
+        Me.txtReadCount.Name = "txtReadCount"
+        Me.txtReadCount.ReadOnly = True
+        Me.txtReadCount.Size = New System.Drawing.Size(150, 20)
+        Me.txtReadCount.TabIndex = 73
+        Me.txtReadCount.Text = "0"
+        '
+        'txtModule
+        '
+        Me.txtModule.Location = New System.Drawing.Point(101, 7)
+        Me.txtModule.Name = "txtModule"
+        Me.txtModule.Size = New System.Drawing.Size(150, 20)
+        Me.txtModule.TabIndex = 72
+        '
+        'label2
+        '
+        Me.label2.AutoSize = True
+        Me.label2.Location = New System.Drawing.Point(12, 47)
+        Me.label2.Name = "label2"
+        Me.label2.Size = New System.Drawing.Size(66, 13)
+        Me.label2.TabIndex = 71
+        Me.label2.Text = "Read count:"
+        '
+        'label1
+        '
+        Me.label1.AutoSize = True
+        Me.label1.Location = New System.Drawing.Point(12, 10)
+        Me.label1.Name = "label1"
+        Me.label1.Size = New System.Drawing.Size(74, 13)
+        Me.label1.TabIndex = 70
+        Me.label1.Text = "Module name:"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 500
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(443, 403)
+        Me.Controls.Add(Me.panelDO)
+        Me.Controls.Add(Me.buttonStart)
+        Me.Controls.Add(Me.txtReadCount)
+        Me.Controls.Add(Me.txtModule)
+        Me.Controls.Add(Me.label2)
+        Me.Controls.Add(Me.label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Form1"
+        Me.Text = "Adam6015_17_18 sample program (VB)"
+        Me.panelDO.ResumeLayout(False)
+        Me.panelDO.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Private WithEvents panelDO As System.Windows.Forms.Panel
+    Private WithEvents txtCh5 As System.Windows.Forms.TextBox
+    Private WithEvents btnCh5 As System.Windows.Forms.Button
+    Private WithEvents txtCh4 As System.Windows.Forms.TextBox
+    Private WithEvents btnCh4 As System.Windows.Forms.Button
+    Private WithEvents txtCh3 As System.Windows.Forms.TextBox
+    Private WithEvents btnCh3 As System.Windows.Forms.Button
+    Private WithEvents txtCh2 As System.Windows.Forms.TextBox
+    Private WithEvents btnCh2 As System.Windows.Forms.Button
+    Private WithEvents txtCh1 As System.Windows.Forms.TextBox
+    Private WithEvents btnCh1 As System.Windows.Forms.Button
+    Private WithEvents txtCh0 As System.Windows.Forms.TextBox
+    Private WithEvents btnCh0 As System.Windows.Forms.Button
+    Private WithEvents buttonStart As System.Windows.Forms.Button
+    Private WithEvents txtReadCount As System.Windows.Forms.TextBox
+    Private WithEvents txtModule As System.Windows.Forms.TextBox
+    Private WithEvents label2 As System.Windows.Forms.Label
+    Private WithEvents label1 As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,293 @@
+﻿Imports Advantech.Adam
+Imports System.Net.Sockets
+
+Public Class Form1
+    Private m_bStart As Boolean
+    Private adamModbus As AdamSocket
+    Private adamUDP As AdamSocket
+    Private m_Adam6000Type As Adam6000Type
+    Private m_szIP As String
+    Private m_szFwVersion As String
+    Const m_Adam6000M3FwVer As Integer = 5
+    Private m_DeviceFwVer As Integer = 4
+    Private m_iPort As Integer
+    Private m_iCount As Integer
+    Private m_iAiTotal As Integer, m_iDoTotal As Integer
+    Private m_bChEnabled() As Boolean
+    Private m_byRange As Byte()
+    Private m_usRange As UShort() 'for newer version Adam
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        m_bStart = False   ' the action stops at the beginning
+        m_szIP = "169.254.219.134" ' modbus slave IP address
+        m_iPort = 502    ' modbus TCP port is 502
+        adamModbus = New AdamSocket
+        adamModbus.SetTimeout(1000, 1000, 1000) ' set timeout for TCP
+        adamUDP = New AdamSocket
+        adamUDP.SetTimeout(1000, 1000, 1000) ' set timeout for UDP
+
+        m_Adam6000Type = Adam6000Type.Adam6060
+        ' the sample is for ADAM-6015
+        'm_Adam6000Type = Adam6000Type.Adam6017 ' the sample is for ADAM-6017
+        'm_Adam6000Type = Adam6000Type.Adam6018 ' the sample is for ADAM-6018
+
+        adamUDP.Connect(AdamType.Adam6000, m_szIP, ProtocolType.Udp)
+        If (adamUDP.Configuration().GetFirmwareVer(m_szFwVersion) = True) Then
+            m_DeviceFwVer = Integer.Parse(m_szFwVersion.Trim().Substring(0, 1))
+        End If
+        adamUDP.Disconnect()
+
+        m_iAiTotal = AnalogInput.GetChannelTotal(m_Adam6000Type)
+        m_iDoTotal = DigitalOutput.GetChannelTotal(m_Adam6000Type)
+
+        txtModule.Text = m_Adam6000Type.ToString()
+        m_bChEnabled = New Boolean(m_iAiTotal - 1) {}
+
+        If (m_DeviceFwVer < m_Adam6000M3FwVer) Then
+            m_byRange = New Byte(m_iAiTotal - 1) {}
+        Else 'for newer version Adam
+            m_usRange = New UShort(m_iAiTotal - 1) {}
+        End If
+
+        ' arrange channel text box
+
+        If (m_Adam6000Type = Adam6000Type.Adam6015) Then
+            ' Channel
+
+            ' DO
+            panelDO.Visible = False
+        ElseIf (m_Adam6000Type = Adam6000Type.Adam6017) Then
+            ' DO
+            btnCh2.Visible = False
+            txtCh2.Visible = False
+            btnCh3.Visible = False
+            txtCh3.Visible = False
+            btnCh4.Visible = False
+            txtCh4.Visible = False
+            btnCh5.Visible = False
+            txtCh5.Visible = False
+
+        Else  'Adam6018
+        End If
+    End Sub
+
+    Private Sub Form1_FormClosing(ByVal sender As System.Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles MyBase.FormClosing
+        adamUDP.Disconnect()
+
+        If (m_bStart = True) Then
+            Timer1.Enabled = False
+            adamModbus.Disconnect() ' disconnect slave
+        End If
+    End Sub
+
+    Private Sub RefreshChannelRangeByteFormat(ByVal i_iChannel As Integer)
+        Dim byRange As Byte
+        If (adamModbus.AnalogInput().GetInputRange(i_iChannel, byRange) = True) Then
+            m_byRange(i_iChannel) = byRange
+        Else
+            txtReadCount.Text += "GetRangeCode() failed"
+        End If
+    End Sub
+
+    Private Sub RefreshChannelRangeUshortFormat(ByVal i_iChannel As Integer)
+        'for newer version Adam
+        Dim usRange As UShort
+        If (adamModbus.AnalogInput().GetInputRange(i_iChannel, usRange) = True) Then
+            m_usRange(i_iChannel) = usRange
+        Else
+            txtReadCount.Text += "GetRangeCode() failed"
+        End If
+    End Sub
+
+    Private Sub RefreshChannelEnabled()
+        Dim bEnabled() As Boolean
+
+        If (adamModbus.AnalogInput().GetChannelEnabled(m_iAiTotal, bEnabled) = True) Then
+            Array.Copy(bEnabled, 0, m_bChEnabled, 0, m_iAiTotal)
+
+            If (m_bChEnabled(0) = False) Then
+                txtCh0.Text = ""
+            End If
+            If (m_bChEnabled(1) = False) Then
+                txtCh1.Text = ""
+            End If
+            If (m_bChEnabled(2) = False) Then
+                txtCh2.Text = ""
+            End If
+            If (m_bChEnabled(3) = False) Then
+                txtCh3.Text = ""
+            End If
+            If (m_bChEnabled(4) = False) Then
+                txtCh4.Text = ""
+            End If
+            If (m_bChEnabled(5) = False) Then
+                txtCh5.Text = ""
+            End If
+
+        Else
+            txtReadCount.Text += "GetChannelEnabled() failed"
+        End If
+    End Sub
+
+    Private Sub buttonStart_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles buttonStart.Click
+        If (m_bStart = True) Then ' was started
+            panelDO.Enabled = False
+            m_bStart = False  ' starting flag
+            Timer1.Enabled = False ' disable timer
+            adamModbus.Disconnect() ' disconnect slave
+            buttonStart.Text = "Start"
+        Else ' was stoped
+            If (adamModbus.Connect(m_szIP, ProtocolType.Tcp, m_iPort)) Then
+
+                panelDO.Enabled = True
+                m_iCount = 0 ' reset the reading counter
+                Timer1.Enabled = True ' enable timer
+                buttonStart.Text = "Stop"
+                m_bStart = True ' starting flag
+
+                If (m_DeviceFwVer < m_Adam6000M3FwVer) Then
+
+                    If (m_Adam6000Type = Adam6000Type.Adam6017 Or m_Adam6000Type = Adam6000Type.Adam6018) Then
+                        RefreshChannelRangeByteFormat(7)
+                    End If
+
+                    RefreshChannelRangeByteFormat(6)
+                    RefreshChannelRangeByteFormat(5)
+                    RefreshChannelRangeByteFormat(4)
+                    RefreshChannelRangeByteFormat(3)
+                    RefreshChannelRangeByteFormat(2)
+                    RefreshChannelRangeByteFormat(1)
+                    RefreshChannelRangeByteFormat(0)
+
+                Else
+
+                    'for newer version Adam
+                    If (m_Adam6000Type = Adam6000Type.Adam6017 Or m_Adam6000Type = Adam6000Type.Adam6018) Then
+                        RefreshChannelRangeUshortFormat(7)
+                    End If
+                    RefreshChannelRangeUshortFormat(6)
+                    RefreshChannelRangeUshortFormat(5)
+                    RefreshChannelRangeUshortFormat(4)
+                    RefreshChannelRangeUshortFormat(3)
+                    RefreshChannelRangeUshortFormat(2)
+                    RefreshChannelRangeUshortFormat(1)
+                    RefreshChannelRangeUshortFormat(0)
+
+                End If
+
+                RefreshChannelEnabled()
+            Else
+                MessageBox.Show("Connect to " + m_szIP + " failed", "Error")
+            End If
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Timer1.Enabled = False
+
+        m_iCount = m_iCount + 1 ' increment the reading counter
+        txtReadCount.Text = "Read coil " + m_iCount.ToString() + " times..."
+
+        If (m_DeviceFwVer < m_Adam6000M3FwVer) Then
+
+        Else
+
+        End If
+
+        RefreshDO()
+
+        Timer1.Enabled = True
+    End Sub
+
+    Private Sub RefreshDO()
+        Dim iStart As Integer = 17
+        Dim bData() As Boolean = New Boolean(m_iDoTotal - 1) {}
+
+        If (m_iDoTotal = 0) Then
+
+        Else
+            If (adamModbus.Modbus().ReadCoilStatus(iStart, m_iDoTotal, bData)) Then
+
+                If (m_iDoTotal > 0) Then
+                    txtCh0.Text = bData(0).ToString()
+                End If
+                If (m_iDoTotal > 1) Then
+                    txtCh1.Text = bData(1).ToString()
+                End If
+                If (m_iDoTotal > 2) Then
+                    txtCh2.Text = bData(2).ToString()
+                End If
+                If (m_iDoTotal > 3) Then
+                    txtCh3.Text = bData(3).ToString()
+                End If
+                If (m_iDoTotal > 4) Then
+                    txtCh4.Text = bData(4).ToString()
+                End If
+                If (m_iDoTotal > 5) Then
+                    txtCh5.Text = bData(5).ToString()
+                End If
+
+            Else
+                txtCh0.Text = "Fail"
+                txtCh1.Text = "Fail"
+                txtCh2.Text = "Fail"
+                txtCh3.Text = "Fail"
+                txtCh4.Text = "Fail"
+                txtCh5.Text = "Fail"
+
+            End If
+        End If
+    End Sub
+
+
+
+
+
+
+
+
+
+
+    Private Sub btnCh_Click(ByVal i_iCh As Integer, ByVal txtBox As TextBox)
+        Dim iOnOff As Integer, iStart As Integer = 17 + i_iCh
+
+        Timer1.Enabled = False
+        If (txtBox.Text = "True") Then ' was ON, now set to OFF
+            iOnOff = 0
+        Else
+            iOnOff = 1
+        End If
+        If (adamModbus.Modbus().ForceSingleCoil(iStart, iOnOff)) Then
+            RefreshDO()
+        Else
+            MessageBox.Show("Set digital output failed!", "Error")
+        End If
+        Timer1.Enabled = True
+    End Sub
+
+    Private Sub btnCh0_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCh0.Click
+        btnCh_Click(0, txtCh0)
+    End Sub
+
+    Private Sub btnCh1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCh1.Click
+        btnCh_Click(1, txtCh1)
+    End Sub
+
+    Private Sub btnCh2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCh2.Click
+        btnCh_Click(2, txtCh2)
+    End Sub
+
+    Private Sub btnCh3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCh3.Click
+        btnCh_Click(3, txtCh3)
+    End Sub
+
+    Private Sub btnCh4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCh4.Click
+        btnCh_Click(4, txtCh4)
+    End Sub
+
+    Private Sub btnCh5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCh5.Click
+        btnCh_Click(5, txtCh5)
+    End Sub
+
+
+End Class
